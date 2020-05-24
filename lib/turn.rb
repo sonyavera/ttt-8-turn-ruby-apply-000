4ec8ba9fc0df402d7026707ea6fda5d98618280a
Sonya Gould <sonyavgould@gmail.com>
2020-05-24 03:54:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,7 +17,7 @@ end
 
 
 def valid_move?(board, index)
-  if (index.between?(0, 8) == true && position_taken?(board, index) = false)
+  if (index.between?(0, 8) == true && position_taken?(board, index) == false)
        true
      else
        false
