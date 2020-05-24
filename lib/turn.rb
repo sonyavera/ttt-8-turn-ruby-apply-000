9ce4125c0eaa368ee3a7cc15d36f33ad1fee2270
Sonya Gould <sonyavgould@gmail.com>
2020-05-24 03:46:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,2 +1,15 @@
 def turn 
   
+  
+  
+  
+  
+#   #ask for input
+# get input
+# convert input to index
+# if index is valid
+#   make the move for index
+#   show the board
+# else
+#   ask for input again until you get a valid input
+# end
