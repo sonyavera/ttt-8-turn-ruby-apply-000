3d2c1dd5f28980edaa5995791aadb2334a15887a
Sonya Gould <sonyavgould@gmail.com>
2020-05-24 03:48:27 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,6 +4,26 @@ def turn
   
   
   
+def valid_move?(board, index)
+  if index.between?(0, 8) == true && position_taken?(board, index) = false
+       true
+     else
+       false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  else
+    true
+  end
+end
+  
+  
+  
 #   #ask for input
 # get input
 # convert input to index
