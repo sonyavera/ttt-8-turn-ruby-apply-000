b29a3b83111b387fdd9def922b57ffc29d0205d6
Sonya Gould <sonyavgould@gmail.com>
2020-05-24 04:28:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,7 +4,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index) == true
     move(board, index, position ="X")
-    puts board
+    puts display_board(board)
   else
     turn(board)
   end
